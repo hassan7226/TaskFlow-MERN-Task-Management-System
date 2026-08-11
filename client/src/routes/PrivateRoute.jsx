4c7e6ac8d96fb66172cee2ab17c8614b3ba92f25
Hassan Arshad <hassanarshad7226@gmail.com>
2026-08-12 00:45:05 +0500

    setuped frontend

---
+++ client/src/routes/PrivateRoute.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const PrivateRoute = ({ allowedRoles }) => {
+  return (
+    <div>
+      <Outlet />
+    </div>
+  )
+}
+
+export default PrivateRoute
